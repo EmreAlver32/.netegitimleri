faf8c231c914c4104efc45687a48c37a4df9c757
EmreAlver32 <emre.a32@hotmail.com>
2023-12-16 23:45:55 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int s1, s2, toplam;
+Console.Write("Birinci sayıyı giriniz:");
+s1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("İkinci sayıyı giriniz");
+s2 = Convert.ToInt32(Console.ReadLine());
+toplam = s1 + s2;
+Console.WriteLine("Gridiginiz sayilarin toplamı = {0}", toplam);
+Console.ReadKey();
+
+
+
+
